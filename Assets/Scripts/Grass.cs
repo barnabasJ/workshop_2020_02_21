0976b1218cdcb192fcf77e74f6b500084d2ab04f
Ellarddekoeijer <40793289+Ellarddekoeijer@users.noreply.github.com>
2020-02-21 14:13:46 +0100

    Added grass script

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform) print("Foreach loop: " + child);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
